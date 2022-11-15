7ef541ae0e09e0933f8993de0ac383a37035c682
Tanusree24 <tanusree241996@gmail.com>
2022-11-15 11:29:35 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>HELLO TANUSREE</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 50,
+    fontFamily: 'Marhey-Regular',
+  },
+});
